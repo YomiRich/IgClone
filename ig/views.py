802035611af802da54319d91658056d353c54b7e
Yomi Rich <ymueni@gmail.com>
2019-10-14 10:21:33 +0300

    Add profile view function

--- ig/views.py
+++ ig/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def profile_of_day(request):
+    profile = Profile.objects.all()
+    images=Image.objects.all()
+    return render(request,'homepage.html',{"images":images, "profile":profile})
