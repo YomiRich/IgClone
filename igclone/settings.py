2f3bce8bc0c2fddeb1d89bb72e002379a1782e6d
Yomi Rich <ymueni@gmail.com>
2019-10-14 11:34:19 +0300

    Install Pylint Django

--- igclone/settings.py
+++ igclone/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'NAME': 'ig',
         'USER': 'moringa',
-    'PASSWORD':'1234',
+    'PASSWORD':'Pekelee1!',
     }
 }
 
