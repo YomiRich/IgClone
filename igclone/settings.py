eaef0f243496a784ca99506d97e00a74720177a0
Yomi Rich <ymueni@gmail.com>
2019-10-14 11:13:42 +0300

    Migrate

--- igclone/settings.py
+++ igclone/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'igclone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'NAME': 'ig',
-        'USER': '<moringa >',
-    'PASSWORD':'<1234>',
+        'USER': 'moringa',
+    'PASSWORD':'1234',
     }
 }
 
